8cca432befbff41b9ee925fb41d411e5c5fb7a8e
Mariahseggebruch <mariahseggebruch@gmail.com>
2023-03-09 12:10:23 -0800

    task 0

--- printf.c
+++ printf.c
@@ -7,6 +7,16 @@ int _printf(const char *format, ...)
 {int i = 0;
 while (format[i] != '\0')
 }
+if (format[i] == '%')
+	(format[i + 1];
+}
+{
+if (format[i + 1] == 's')
+	_pucthar(var_arg)
+}
+{
+if (format[i + 1] == 'c')
+	_putchar(var_arg)
 {	_putchar (format[i]);
 	i++;
 }
