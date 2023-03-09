5f35685657ae298de1186529ab1bb1aa53762625
Jamie897 <jamiedanielle456@gmail.com>
2023-03-09 09:55:47 -0800

    task 0

--- printf.c
+++ printf.c
@@ -3,5 +3,20 @@
 #include <stdio.h>
 
 int _printf(const char *format, ...);
-
+{
+		int i = c
+	while(format[i] != '\0')
+	{	
+		_putchar(format[i]
+			i++;
+			}
+}	
+{	
+	int x = s
+	while(x != null)
+	{
+		_putchar(s)
+		x++
+	}
+}
  
