72a872d62ad1968a396808466a00678fa892addd
Jamie897 <jamiedanielle456@gmail.com>
2023-03-09 11:29:13 -0800

    tasl 0

--- printf.c
+++ printf.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 
 int _printf(const char *format, ...)
-int i = 0;
-while(format[i] != '\0')
-{	_putchar(format[i]);
+
+{int i = 0;
+while (format[i] != '\0')
+}
+{	_putchar (format[i]);
 	i++;
 }
-return(i);
+return (i);
